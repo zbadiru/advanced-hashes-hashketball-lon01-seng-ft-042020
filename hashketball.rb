0f71bc3892f5b9212da942e0db5962b615a7b3e5
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 19:47:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,7 +150,7 @@ end
 
 def team_colors(team_name)
   game_hash. each do |place, team|
-    return game_hash[place][:colors] if team[team_name] == 
+    return game_hash[place][:colors] if team[team_name] == team_name
 
 
 
