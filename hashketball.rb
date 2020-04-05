2ce2fc84dfed8afe3efc4bfbc3e68f009594f299
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 19:38:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,9 +128,9 @@ end
 
 def num_point_scored(sought_player_name)
   game_hash.each do |_player, team|
-  team.each do |attribute, data|
-    next unless attribute == :players
-    data.each do |player|
+    team.each do |attribute, data|
+     next unless attribute == :players
+     data.each do |player|
       return player[:points] if player[:player_name] == sought_player_name
     end
   end
