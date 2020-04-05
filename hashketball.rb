901a625e2b98505102b0026e754992e690e53358
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 19:54:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,8 @@ def player_numbers(team_name)
     next unless team[:team_name] == team_name
     team.each do |attribute, data|
       next unless attribute == :players
-      data.each do |
+      data.each do |data|
+        
 
 
 
