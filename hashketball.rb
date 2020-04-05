eec9efe75f927220335fd43cb75e498039ddf45a
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 20:05:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,7 +194,8 @@ def big_shoe_rebounds
   biggest_shoe = 0 
   num_rebounds = 0 
   game_hash.each do |_team, game_data|
-    game_data[:players]
+    game_data[:players].each do |player|
+      
 
 
 
